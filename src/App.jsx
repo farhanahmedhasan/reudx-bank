518217bcf98433965e9d46212a0889bbf0014188
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-30 14:30:37 +0600

    dispatched createCustomer action creator function from UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import CreateCustomer from "./features/customers/CreateCustomer.jsx";
 import Customer from "./features/customers/Customer.jsx";
 import AccountOperations from "./features/accounts/AccountOperations.jsx";
 import BalanceDisplay from "./features/accounts/BalanceDisplay.jsx";
+import {useSelector} from "react-redux";
 
 function App() {
+    const customerFullName = useSelector(store => store.customer.fullName)
+
     return (
         <div>
             <h1>🏦 The React-Redux Bank ⚛️</h1>
-            <CreateCustomer />
-            <Customer />
-            <AccountOperations />
-            <BalanceDisplay />
+            {customerFullName &&
+                <>
+                    <Customer />
+                    <AccountOperations />
+                    <BalanceDisplay />
+                </>
+            }
+
+            {!customerFullName && <CreateCustomer />}
         </div>
     );
 }
