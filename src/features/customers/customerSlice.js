bb4f48f6ca086243fe4331fc3dda8d29aa24ef9c
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-10-03 11:11:08 +0600

    converted customerSlice to redux toolkit

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,48 +1,43 @@
 // Initial State
-const initialStateCustomer = {
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
     fullName: "",
     nationalID: "",
     createdAt:""
 }
 
-// Reducer
-export default function customerReducer(state = initialStateCustomer,action){
-    switch (action.type){
-        case "customer/createCustomer":
-            return {
-                ...state,
-                fullName: action.payload.fullName,
-                nationalID: action.payload.nationalID,
-                createdAt: action.payload.createdAt
+const customerSlice = createSlice({
+    name: "customer",
+    initialState,
+    reducers: {
+        createCustomer: {
+            prepare(fullName, nationalID){
+                return {
+                    payload: {
+                        fullName,
+                        nationalID,
+                        createdAt: new Date().toISOString()
+                    }
+                }
+            },
+
+            reducer (state, action){
+                state.fullName = action.payload.fullName
+                state.nationalID = action.payload.nationalID
+                state.createdAt = action.payload.createdAt
             }
+        },
 
-        case "customer/updateName":
-            return {
-                ...state,
-                fullName: action.payload
-            }
-
-        default :
-            return state
-    }
-}
-
-// Action Creator Functions
-function createCustomer(fullName, nationalID){
-    return {
-        type : "customer/createCustomer",
-        payload: {
-            fullName,
-            nationalID,
-            createdAt: new Date().toISOString()
+        updateName(state, action){
+            state.fullName = action.payload
         }
     }
-}
-function updateName(fullName){
-    return {
-        type: "customer/updateName",
-        payload: fullName
-    }
-}
+})
 
-export {createCustomer, updateName}
+export const {
+    createCustomer,
+    updateName
+} = customerSlice.actions
+
+export default customerSlice.reducer
